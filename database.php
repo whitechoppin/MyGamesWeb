1b7a39ea823eb941f2fd1f4b02896e6c72937304
Alexander Wibowo <alexanderwibowo.aw@gmail.com>
2019-06-17 00:25:15 +0800

    Update database.php

--- database.php
+++ database.php
@@ -17,18 +17,12 @@ class Database
        // One connection through whole application
        if ( null == self::$cont )
        {     
-        try
-        {
-          // self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
-          // self::$cont = new PDO('sqlsrv:server=tcp:mygamesweb.database.windows.net,1433;Database=permainan','alexwibowo','08Maret2017');}
-            $cont=mysqli_init(); 
-            [mysqli_ssl_set($cont, NULL, NULL, {ca-cert filename}, NULL, NULL);] 
-            mysqli_real_connect($cont, 'mygamesweb.database.windows.net', 'alexwibowo', '08Maret2017', 'permainan', 1433);
-
-        catch(PDOException $e)
-        {
-          die($e->getMessage()); 
-        }
+        try {
+              self::$cont = new PDO('sqlsrv:server=tcp:mygamesweb.database.windows.net,1433;Database=permainan','alexwibowo','08Maret2017');
+              self::$cont ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          } catch (PDOException $e) {
+            die($e->getMessage()); 
+          }
        }
        return self::$cont;
     }
