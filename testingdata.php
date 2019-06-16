44f1fe64bc7c7ff67c1f73b54b50bdfd0701b567
Alexander Wibowo <alexanderwibowo.aw@gmail.com>
2019-06-17 01:21:19 +0800

    Create testingdata.php

---
+++ testingdata.php
@@ -0,0 +1,11 @@
+<?php
+    $serverName = "mygamesweb.database.windows.net"; // update me
+    // $serverName = "tcp:ttyir96emw.database.windows.net,1433";
+    $connectionOptions = array(
+        "Database" => "permainan", // update me
+        "Uid" => "alexwibowo", // update me
+        "PWD" => "08Maret2017" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+?>
