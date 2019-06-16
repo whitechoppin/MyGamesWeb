d5e2f55a4e356ee25f64c28566d5140fd127c393
Alexander Wibowo <alexanderwibowo@hotmail.com>
2019-06-17 01:54:09 +0800

    Update testingdata.php

--- testingdata.php
+++ testingdata.php
@@ -1,6 +1,6 @@
 <?php
-    $serverName = "mygamesweb.database.windows.net"; // update me
-    // $serverName = "tcp:ttyir96emw.database.windows.net,1433";
+//     $serverName = "mygamesweb.database.windows.net";
+    $serverName = "tcp:mygamesweb.database.windows.net,1433";
     $connectionOptions = array(
         "Database" => "permainan", // update me
         "Uid" => "alexwibowo", // update me
@@ -12,28 +12,28 @@
         echo "Connected!"
         
     //Insert Query
-    echo ("Inserting a new row into table" . PHP_EOL);
-    $tsql= "INSERT INTO permainan.game (nama, genre, negara, produser) VALUES (?,?,?,?);";
-    $params = array('Dynasti Warrior','Beat em all','china','koei');
-    $getResults= sqlsrv_query($conn, $tsql, $params);
-    $rowsAffected = sqlsrv_rows_affected($getResults);
-    if ($getResults == FALSE or $rowsAffected == FALSE)
-        die(FormatErrors(sqlsrv_errors()));
-    echo ($rowsAffected. " row(s) inserted: " . PHP_EOL);
+//     echo ("Inserting a new row into table" . PHP_EOL);
+//     $tsql= "INSERT INTO permainan.game (nama, genre, negara, produser) VALUES (?,?,?,?);";
+//     $params = array('Dynasti Warrior','Beat em all','china','koei');
+//     $getResults= sqlsrv_query($conn, $tsql, $params);
+//     $rowsAffected = sqlsrv_rows_affected($getResults);
+//     if ($getResults == FALSE or $rowsAffected == FALSE)
+//         die(FormatErrors(sqlsrv_errors()));
+//     echo ($rowsAffected. " row(s) inserted: " . PHP_EOL);
 
-    sqlsrv_free_stmt($getResults);
+//     sqlsrv_free_stmt($getResults);
 
-    function FormatErrors( $errors )
-    {
-        /* Display errors. */
-        echo "Error information: ";
+//     function FormatErrors( $errors )
+//     {
+//         /* Display errors. */
+//         echo "Error information: ";
 
-        foreach ( $errors as $error )
-        {
-            echo "SQLSTATE: ".$error['SQLSTATE']."";
-            echo "Code: ".$error['code']."";
-            echo "Message: ".$error['message']."";
-        }
-    }
+//         foreach ( $errors as $error )
+//         {
+//             echo "SQLSTATE: ".$error['SQLSTATE']."";
+//             echo "Code: ".$error['code']."";
+//             echo "Message: ".$error['message']."";
+//         }
+//     }
 
 ?>
