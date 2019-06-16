5df4a3cf33ec0f02c5d44475b6db0d90e079a1bf
Alexander Wibowo <alexanderwibowo@hotmail.com>
2019-06-17 01:47:30 +0800

    Update testingdata.php

--- testingdata.php
+++ testingdata.php
@@ -10,4 +10,17 @@
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     if($conn)
         echo "Connected!"
+        
+    //Insert Query
+    echo ("Inserting a new row into table" . PHP_EOL);
+    $tsql= "INSERT INTO permainan.game (nama, genre, negara, produser) VALUES (1,1,1,1);";
+    $params = array('Dynasti Warrior','Beat em all','china','koei');
+    $getResults= sqlsrv_query($conn, $tsql, $params);
+    $rowsAffected = sqlsrv_rows_affected($getResults);
+    if ($getResults == FALSE or $rowsAffected == FALSE)
+        die(FormatErrors(sqlsrv_errors()));
+    echo ($rowsAffected. " row(s) inserted: " . PHP_EOL);
+
+    sqlsrv_free_stmt($getResults);
+
 ?>
