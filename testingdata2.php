958a1bd505122e098c3d6bc3bee6a97fb9de24dc
Alexander Wibowo <alexanderwibowo@hotmail.com>
2019-06-17 22:24:07 +0800

    Update testingdata2.php

--- testingdata2.php
+++ testingdata2.php
@@ -5,109 +5,64 @@
 <body>  
 <form method="post" action="?action=add" enctype="multipart/form-data" >  
     nama
-    <input type="text" name="t_emp_id" id="t_emp_id"/></br>  
+    <input type="text" name="nama" id="nama"/></br>  
     genre 
-    <input type="text" name="t_name" id="t_name"/></br>  
+    <input type="text" name="genre" id="genre"/></br>  
     negara
-    <input type="text" name="t_education" id="t_education"/></br>  
+    <input type="text" name="negara" id="negara"/></br>  
     produser
-    <input type="text" name="t_email" id="t_email"/></br>  
+    <input type="text" name="produser" id="produser"/></br>  
     <input type="submit" name="submit" value="Submit" />  
 </form>  
 <?php  
-/*Connect using SQL Server authentication.*/  
-    // $serverName = "mygamesweb.database.windows.net";
     $serverName = "tcp:mygamesweb.database.windows.net,1433";
     $connectionOptions = array(
         "Database" => "permainan", // update me
         "Uid" => "alexwibowo", // update me
         "PWD" => "08Maret2017" // update me
     ); 
-$conn = sqlsrv_connect($serverName, $connectionOptions);  
+    $conn = sqlsrv_connect($serverName, $connectionOptions);  
   
-if ($conn === false)  
+    if ($conn === false)  
     {  
-    die(print_r(sqlsrv_errors() , true));  
+        die(print_r(sqlsrv_errors() , true));  
     }  
   
-if (isset($_GET['action']))  
+    if (isset($_GET['action']))  
     {  
-    if ($_GET['action'] == 'add')  
+        if ($_GET['action'] == 'add')  
         {  
-        /*Insert data.*/  
-        $insertSql = "INSERT INTO game (nama,genre,negara,produser) VALUES (?,?,?,?)";  
-        $params = array(&$_POST['t_emp_id'], &$_POST['t_name'], &$_POST['t_education'], &$_POST['t_email']  
-        );  
-        $stmt = sqlsrv_query($conn, $insertSql, $params);  
-        if ($stmt === false)  
-            {  
-            /*Handle the case of a duplicte e-mail address.*/  
-            $errors = sqlsrv_errors();  
-            if ($errors[0]['code'] == 2601)  
+            /*Insert data.*/  
+            $insertSql = "INSERT INTO game (nama,genre,negara,produser) VALUES (?,?,?,?)";  
+            $params = array(&$_POST['nama'], &$_POST['genre'], &$_POST['negara'], &$_POST['produser']  
+            );  
+            $stmt = sqlsrv_query($conn, $insertSql, $params);  
+            if ($stmt === false)  
                 {  
-                echo "The e-mail address you entered has already been used.</br>";  
-                }  
-  
-            /*Die if other errors occurred.*/  
-              else  
+                $errors = sqlsrv_errors();  
+                if ($errors[0]['code'] == 2601)  
                 {  
-                die(print_r($errors, true));  
+                    echo "The data you entered is fail.</br>";  
                 }  
-            }  
-        else  
+                /*Die if other errors occurred.*/  
+                else  
+                    {  
+                    die(print_r($errors, true));  
+                    }  
+                }  
+            else  
             {  
-            echo "Registration complete.</br>";  
-            $rowsAffected = sqlsrv_rows_affected($stmt);
-            if ($stmt == FALSE or $rowsAffected == FALSE)
-                die(FormatErrors(sqlsrv_errors()));
-            echo ($rowsAffected. " row(s) inserted: " . PHP_EOL);
+                echo "Registration complete.</br>";  
+                $rowsAffected = sqlsrv_rows_affected($stmt);
+                if ($stmt == FALSE or $rowsAffected == FALSE)
+                    die(FormatErrors(sqlsrv_errors()));
+                echo ($rowsAffected. " row(s) inserted: " . PHP_EOL);
 
-            sqlsrv_free_stmt($stmt);
-            
-            //Read Query
-            $tsql= "SELECT * FROM game;";
-            $getResults= sqlsrv_query($conn, $tsql);
-            echo ("Reading data from table" . PHP_EOL);
-            if ($getResults == FALSE)
-                die(FormatErrors(sqlsrv_errors()));
-            while ($row = sqlsrv_fetch_array($getResults)) {
-                echo ($row['id'] . " " . $row['nama'] . " " . $row['genre'] . PHP_EOL);
-
-            }
-            sqlsrv_free_stmt($getResults);
-            header("Location: index.php");
-      
+                sqlsrv_free_stmt($stmt);
             }  
-        }  
+        } 
+        header("Location: index.php"); 
     }  
-  
-/*Display registered people.*/  
-/*$sql = "SELECT * FROM empTable ORDER BY name"; 
-$stmt = sqlsrv_query($conn, $sql); 
-if($stmt === false) 
-{ 
-die(print_r(sqlsrv_errors(), true)); 
-} 
- 
-if(sqlsrv_has_rows($stmt)) 
-{ 
-print("<table border='1px'>"); 
-print("<tr><td>Emp Id</td>"); 
-print("<td>Name</td>"); 
-print("<td>education</td>"); 
-print("<td>Email</td></tr>"); 
- 
-while($row = sqlsrv_fetch_array($stmt)) 
-{ 
- 
-print("<tr><td>".$row['emp_id']."</td>"); 
-print("<td>".$row['name']."</td>"); 
-print("<td>".$row['education']."</td>"); 
-print("<td>".$row['email']."</td></tr>"); 
-} 
- 
-print("</table>"); 
-}*/  
 ?>  
 </body>  
 </html>  
