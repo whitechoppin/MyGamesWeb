3e45b9dac27e4f9f52b9db695ccf1a1e8fc77fa7
Alexander Wibowo <alexanderwibowo@hotmail.com>
2019-06-18 00:32:16 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,37 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <link   href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
-</head>
- 
-<body>
-    <div class="container">
-     
-                <div class="span10 offset1">
-                    <div class="row">
-                        <h3>Delete a Customer</h3>
-                    </div>
-                    <?php
-                        $id = null;
-                        if ( !empty($_GET['id'])) {
-                            $id = $_REQUEST['id'];
-                        }
-                    ?>
-                    <form class="form-horizontal" action="?action=delete" method="post" enctype="multipart/form-data">
-                      <input type="hidden" name="id" value="<?php echo $id;?>"/>
-                      <p class="alert alert-error">Are you sure to delete ?</p>
-                      <div class="form-actions">
-                          <button type="submit" class="btn btn-danger">Yes</button>
-                          <a class="btn" href="index.php">No</a>
-                        </div>
-                    </form>
-                </div>
-                 
-    </div> <!-- /container -->
-</body>
+
 <?php
+
     $serverName = "tcp:mygamesweb.database.windows.net,1433";
     $connectionOptions = array(
         "Database" => "permainan", // update me
@@ -45,11 +16,18 @@
         die(print_r(sqlsrv_errors() , true));  
     } 
     
-    if (isset($_GET['action']))  
-    {  
-        if ($_GET['action'] == 'delete')  
-        {  
-            $sql = "DELETE FROM game WHERE id = ?";
+    $id = 0;
+     
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( !empty($_POST)) {
+        // keep track post values
+        $id = $_POST['id'];
+         
+        // delete data
+        $sql = "DELETE FROM game WHERE id = ?";
             $params = array($id);  
             $stmt = sqlsrv_query($conn, $sql, $params);
             if ($stmt)  
@@ -61,8 +39,33 @@
     
                 sqlsrv_free_stmt($stmt);
             }  
-            header("Location: index.php");
-        }
+        $conn = null;
+        header("Location: index.php");
     }
 ?>
+
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Delete a Customer</h3>
+                    </div>
+                    <form class="form-horizontal" action="delete.php" method="post">
+                      <input type="hidden" name="id" value="<?php echo $id;?>"/>
+                      <p class="alert alert-error">Are you sure to delete ?</p>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-danger">Yes</button>
+                          <a class="btn" href="index.php">No</a>
+                        </div>
+                    </form>
+                </div>
+    </div> <!-- /container -->
+</body>
 </html>
