b4dc3a8c5279e8a0d024074342078347f6e9c0e7
Alexander Wibowo <alexanderwibowo@hotmail.com>
2019-06-18 00:09:09 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,12 @@
-$con=mysqli_init(); [mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);] mysqli_real_connect($con, {your_host}, {username@servername}, {your_password}, {your_database}, {your_port});
+<?php
+    $serverName = "tcp:mygamesweb.database.windows.net,1433";
+    $connectionOptions = array(
+        "Database" => "permainan", // update me
+        "Uid" => "alexwibowo", // update me
+        "PWD" => "08Maret2017" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if($conn)
+        echo "Connected!"
+?>
