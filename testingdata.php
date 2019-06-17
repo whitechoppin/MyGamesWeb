3e150dd1a7b505bb42c283ce77d3442d302ed8fc
Alexander Wibowo <alexanderwibowo@hotmail.com>
2019-06-18 00:08:49 +0800

    Update testingdata.php

--- testingdata.php
+++ testingdata.php
@@ -1,5 +1,4 @@
 <?php
-//     $serverName = "mygamesweb.database.windows.net";
     $serverName = "tcp:mygamesweb.database.windows.net,1433";
     $connectionOptions = array(
         "Database" => "permainan", // update me
@@ -10,30 +9,4 @@
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     if($conn)
         echo "Connected!"
-    
-    //Insert Query
-//     echo ("Inserting a new row into table" . PHP_EOL);
-//     $tsql= "INSERT INTO permainan.game (nama, genre, negara, produser) VALUES (?,?,?,?);";
-//     $params = array('Dynasti Warrior','Beat em all','china','koei');
-//     $getResults= sqlsrv_query($conn, $tsql, $params);
-//     $rowsAffected = sqlsrv_rows_affected($getResults);
-//     if ($getResults == FALSE or $rowsAffected == FALSE)
-//         die(FormatErrors(sqlsrv_errors()));
-//     echo ($rowsAffected. " row(s) inserted: " . PHP_EOL);
-
-//     sqlsrv_free_stmt($getResults);
-
-//     function FormatErrors( $errors )
-//     {
-//         /* Display errors. */
-//         echo "Error information: ";
-
-//         foreach ( $errors as $error )
-//         {
-//             echo "SQLSTATE: ".$error['SQLSTATE']."";
-//             echo "Code: ".$error['code']."";
-//             echo "Message: ".$error['message']."";
-//         }
-//     }
-
 ?>
