3500859be4eb965db51cdfd7ccf926949a931c3e
Alexander Wibowo <alexanderwibowo@hotmail.com>
2019-06-17 22:32:49 +0800

    Update create.php

--- create.php
+++ create.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,61 +5,44 @@
     <link   href="css/bootstrap.min.css" rel="stylesheet">
     <script src="js/bootstrap.min.js"></script>
 </head>
+  
+<body>  
+<div class="container">
+    <div class="span10 offset1">
+        <div class="row">
+            <h3>Create a Customer</h3>
+        </div>
+    </div>
+    <form class="form-horizontal"method="post" action="?action=add" enctype="multipart/form-data" >  
+        <div class="control-group">
+            <label class="control-label">Nama</label>
+            <div class="controls">
+                <input type="text" name="nama" id="nama"/>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label">Genre</label>
+            <div class="controls">
+                <input type="text" name="genre" id="genre"/>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label">Negara</label>
+            <div class="controls">
+                <input type="text" name="negara" id="negara"/>      
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label">Produser</label>
+            <div class="controls">
+                <input type="text" name="produser" id="produser"/>
+            </div>
+        </div>
+        <input type="submit" name="submit" value="Submit" />  
+    </form> 
+</div>
  
-<body>
-    <div class="container">
-     
-                <div class="span10 offset1">
-                    <div class="row">
-                        <h3>Create a Customer</h3>
-                    </div>
-             
-                    <form class="form-horizontal" method="post" action="?action=add" enctype="multipart/form-data" >  
-                        <div class="control-group <?php echo !empty($namaError)?'error':'';?>">
-                            <label class="control-label">Nama</label>
-                            <div class="controls">
-                                <input name="nama" type="text"  placeholder="Nama" value="<?php echo !empty($nama)?$nama:'';?>">
-                                <?php if (!empty($namaError)): ?>
-                                    <span class="help-inline"><?php echo $namaError;?></span>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                        <div class="control-group <?php echo !empty($genreError)?'error':'';?>">
-                            <label class="control-label">Genre</label>
-                            <div class="controls">
-                                <input name="genre" type="text" placeholder="Genre" value="<?php echo !empty($genre)?$genre:'';?>">
-                                <?php if (!empty($genreError)): ?>
-                                    <span class="help-inline"><?php echo $genreError;?></span>
-                                <?php endif;?>
-                            </div>
-                        </div>
-                        <div class="control-group <?php echo !empty($negaraError)?'error':'';?>">
-                            <label class="control-label">Negara</label>
-                            <div class="controls">
-                                <input name="negara" type="text"  placeholder="Negara" value="<?php echo !empty($negara)?$negara:'';?>">
-                                <?php if (!empty($negaraError)): ?>
-                                    <span class="help-inline"><?php echo $negaraError;?></span>
-                                <?php endif;?>
-                            </div>
-                        </div>
-                        <div class="control-group <?php echo !empty($produserError)?'error':'';?>">
-                            <label class="control-label">Produser</label>
-                            <div class="controls">
-                                <input name="produser" type="text"  placeholder="produser" value="<?php echo !empty($produser)?$produser:'';?>">
-                                <?php if (!empty($produserError)): ?>
-                                    <span class="help-inline"><?php echo $produserError;?></span>
-                                <?php endif;?>
-                            </div>
-                        </div>
-                        <div class="form-actions">
-                            <button type="submit" class="btn btn-success">Create</button>
-                            <a class="btn" href="index.php">Back</a>
-                        </div>
-                    </form>
-                </div>
-                 
-    </div> 
-    <?php
+<?php  
     $serverName = "tcp:mygamesweb.database.windows.net,1433";
     $connectionOptions = array(
         "Database" => "permainan", // update me
@@ -68,35 +50,47 @@
         "PWD" => "08Maret2017" // update me
     ); 
     $conn = sqlsrv_connect($serverName, $connectionOptions);  
-     
+  
     if ($conn === false)  
     {  
         die(print_r(sqlsrv_errors() , true));  
-    } 
- 
+    }  
+  
     if (isset($_GET['action']))  
     {  
         if ($_GET['action'] == 'add')  
         {  
-                // isi data
-            
-            $sql = "INSERT INTO game (nama,genre,negara,produser) VALUES (?, ?, ? ,?)";  
-            $params = array($nama,$genre,$negara,$produser));  
-            $stmt = sqlsrv_query($conn, $sql, $params);  
-            if ($stmt)  
+            /*Insert data.*/  
+            $insertSql = "INSERT INTO game (nama,genre,negara,produser) VALUES (?,?,?,?)";  
+            $params = array(&$_POST['nama'], &$_POST['genre'], &$_POST['negara'], &$_POST['produser']  
+            );  
+            $stmt = sqlsrv_query($conn, $insertSql, $params);  
+            if ($stmt === false)  
+                {  
+                $errors = sqlsrv_errors();  
+                if ($errors[0]['code'] == 2601)  
+                {  
+                    echo "The data you entered is fail.</br>";  
+                }  
+                /*Die if other errors occurred.*/  
+                else  
+                    {  
+                    die(print_r($errors, true));  
+                    }  
+                }  
+            else  
             {  
-                /*Handle the case of a duplicte e-mail address.*/  
                 echo "Registration complete.</br>";  
                 $rowsAffected = sqlsrv_rows_affected($stmt);
+                if ($stmt == FALSE or $rowsAffected == FALSE)
+                    die(FormatErrors(sqlsrv_errors()));
                 echo ($rowsAffected. " row(s) inserted: " . PHP_EOL);
-    
+
                 sqlsrv_free_stmt($stmt);
             }  
-            header("Location: index.php");
-                
-            
-        }
-    }
-?>
-  </body>
-</html>
+        } 
+        header("Location: index.php"); 
+    }  
+?>  
+</body>  
+</html>  
